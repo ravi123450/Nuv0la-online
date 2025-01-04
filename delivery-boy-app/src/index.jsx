792e41b0030d3a7d9528d0d8ca829464425c2f1f
ravi123450 <141273837+ravi123450@users.noreply.github.com>
2025-01-04 19:06:52 +0530

    Update index.jsx

--- delivery-boy-app/src/index.jsx
+++ delivery-boy-app/src/index.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// Get the root DOM node
-const rootElement = document.getElementById("root");
-
-// Create a root and render the app
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
