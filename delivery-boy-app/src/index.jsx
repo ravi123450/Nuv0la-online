50ec54aaacf35dede8083d9cab516fa875330142
ravi123450 <141273837+ravi123450@users.noreply.github.com>
2025-01-04 18:34:33 +0530

    Update index.jsx

--- delivery-boy-app/src/index.jsx
+++ delivery-boy-app/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 
 // Get the root DOM node
 const rootElement = document.getElementById("root");
