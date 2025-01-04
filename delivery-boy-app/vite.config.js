4b3ddfdf81b6000f0f7807c910d5731141a032ce
ravi123450 <141273837+ravi123450@users.noreply.github.com>
2025-01-04 18:41:49 +0530

    Update vite.config.js

--- delivery-boy-app/vite.config.js
+++ delivery-boy-app/vite.config.js
@@ -1,21 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html'),
-    },
+  server: {
+    port: 3000, // Change as needed
   },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+  build: {
+    outDir: "dist", // Output directory for build
   },
 });
-
